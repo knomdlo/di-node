cc98c06fe8a51f76cecc423a086a8d09645b60e0
banutr <banuprakash.j@thomsonreuters.com>
2017-09-01 18:32:01 +0530

    Adding dependencies

--- bin/sample.js
+++ bin/sample.js
@@ -1,2 +1,5 @@
 #! /usr/bin/env node
 console.log("Hello DI team. This is a simple CLI created with node");
+var sum = parseInt(process.argv[2]) + parseInt(process.argv[3]);
+console.log('*************Magic Magic***********');
+console.log(process.argv[2] + ' + ' + process.argv[3] + ' = ' + sum);
